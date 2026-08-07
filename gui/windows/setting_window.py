b47f01ae2cc0209af98534309e2801ad334a6950
aoberon <oberon.arthur42@gmail.com>
2026-08-07 10:27:02 +0200

    update setting window: add title + button (work in progress) + change spinbox for better usability

--- gui/windows/setting_window.py
+++ gui/windows/setting_window.py
@@ -1,5 +1,5 @@
 from PySide6.QtWidgets import QLabel, QVBoxLayout, QWidget, QPushButton, QFormLayout, QSpinBox, QComboBox
-from PySide6.QtCore import Qt
+from PySide6.QtCore import Qt 
 
 class SettingWindow(QWidget):
 	def __init__(self, go_to_menu_callback):
@@ -9,20 +9,32 @@ class SettingWindow(QWidget):
 
 		layout.setSpacing(10)
 
+		# Button Back
 		self.btn_back = QPushButton("<-")
 		self.btn_back.setObjectName("btn_back_setting")
 		self.btn_back.setProperty("top_btn", True)
 		self.btn_back.setShortcut("Escape")
+		self.btn_back.setFocusPolicy(Qt.FocusPolicy.NoFocus)
 		self.btn_back.clicked.connect(go_to_menu_callback)
 
 		layout.addWidget(self.btn_back, alignment=Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
+		layout.addSpacing(10)
 
-		layout.addSpacing(100)
+	
+        # Title
+		title = QLabel("Setting")
+		title.setObjectName("title-setting")
+		title.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignTop)
 
-		form_widget = QWidget()
-		form_widget.setObjectName("layout_form")
+		layout.addWidget(title)
+		layout.addSpacing(50)
 
-		layout_form = QFormLayout(form_widget)
+
+		# Form Layout
+		form_container = QWidget()
+		form_container.setObjectName("layout_form")
+
+		layout_form = QFormLayout(form_container)
 		layout_form.setFormAlignment(Qt.AlignmentFlag.AlignCenter)
 		layout_form.setLabelAlignment(Qt.AlignmentFlag.AlignCenter)
 
@@ -31,7 +43,9 @@ class SettingWindow(QWidget):
 		self.new_by_day_spin.setValue(20)
 		self.new_by_day_spin.setSingleStep(1)
 		self.new_by_day_spin.setObjectName("spin_new_by_day")
-		self.new_by_day_spin.setFocusPolicy(Qt.FocusPolicy.NoFocus)
+		self.new_by_day_spin.setFocusPolicy(Qt.FocusPolicy.ClickFocus)
+		# self.new_by_day_spin.setKeyboardTracking(False)
+		self.new_by_day_spin.setAccelerated(True)
 
 		layout_form.addRow("Number of new cards by day", self.new_by_day_spin)
 
@@ -41,29 +55,38 @@ class SettingWindow(QWidget):
 		self.lat_spin.setSingleStep(1)
 		self.lat_spin.setSuffix("m")
 		self.lat_spin.setObjectName("spin_lat")
-		self.lat_spin.setFocusPolicy(Qt.FocusPolicy.NoFocus)
+		self.lat_spin.setFocusPolicy(Qt.FocusPolicy.ClickFocus)
+		self.new_by_day_spin.setFocusPolicy(Qt.FocusPolicy.ClickFocus)
+		# self.new_by_day_spin.setKeyboardTracking(False)
+		self.new_by_day_spin.setAccelerated(True)
 
 		layout_form.addRow("Number of new cards by day", self.lat_spin)
 
 		self.new_review_random_combo = QComboBox()
 		self.new_review_random_combo.setObjectName("combo_new_review_random")
 		self.new_review_random_combo.addItems(["True", "False"])
-		self.new_review_random_combo.setFocusPolicy(Qt.FocusPolicy.NoFocus)
+		self.new_review_random_combo.setFocusPolicy(Qt.FocusPolicy.ClickFocus)
 
 		layout_form.addRow("Select new card to review randomly", self.new_review_random_combo)
 		
 		self.new_select_random_combo = QComboBox()
 		self.new_select_random_combo.setObjectName("combo_new_select_random")
 		self.new_select_random_combo.addItems(["True", "False"])
-		self.new_select_random_combo.setFocusPolicy(Qt.FocusPolicy.NoFocus)
+		self.new_select_random_combo.setFocusPolicy(Qt.FocusPolicy.ClickFocus)
 
 		layout_form.addRow("Select new card to review randomly", self.new_select_random_combo)
 
 		layout_form.setVerticalSpacing(40)
 		layout_form.setHorizontalSpacing(40)
 
-		layout.addWidget(form_widget, alignment=Qt.AlignmentFlag.AlignCenter)
+		layout.addWidget(form_container, alignment=Qt.AlignmentFlag.AlignCenter)
+		layout.addSpacing(20)
 
+		btn = QPushButton("Save")
+		btn.setObjectName("btn_start")
+		btn.setFocus()
+
+		layout.addWidget(btn)
 
 		layout.addStretch()
 
